1dfc3e80a2209fe375de4d31cf969a26c6c65ea6
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-06 15:40:11 +0700

    selesai

--- pointerFrindClass_074.cpp
+++ pointerFrindClass_074.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa
+class mahasiswa 
 {
-    int nim;
-    void showNim()
-    {
-	cout << "No Induk = " << nim << endl;
-    }
+public:
+	int nim;
+	void showNim()
+	{
+		cout << "No Induk = " << nim << endl;
+	}
 };
+
 int main() {
-    mahasiswa mhs{ 1 };
+	mahasiswa mhs{ 1 };
 	mhs.showNim();
 
 	mahasiswa& refMhs = mhs;
-    refMhs.nim = 2;
+	refMhs.nim = 2;
 	mhs.showNim();
 
-    mahasiswa* pMhs = &mhs;
-    pMhs->nim = 3;
-    pMhs->showNim();
-
-    return 0;
+	mahasiswa* pMhs = &mhs;
+	pMhs->nim = 3;
+	pMhs->showNim();
+	return 0;
 }
